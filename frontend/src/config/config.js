a5e42b03b9b7596f3a2638f1d90f67af69248d56
MuhammadHaris8171 <yassuopro316@gmail.com>
2025-06-19 23:37:41 +0500

    16th commit

---
+++ frontend/src/config/config.js
@@ -0,0 +1,4 @@
+const backend_url ="https://digitalocean-production-f131.up.railway.app/api"
+// const backend_url ="https://resturant1-xi.vercel.app/api"
+export default backend_url
+// https://resturant1-xi.vercel.app/
